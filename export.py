d59957f53d492ae368f150961dd50c3280bed00d
Charles Lenk <lenkc@Charless-MacBook-Air.local>
2025-11-15 17:30:18 -0800

    Improve export naming

--- export.py
+++ export.py
@@ -3,7 +3,7 @@ from scad_export.exportable import Folder, Model
 
 files=Folder(
     name='scad_export/handy_closet_hook',
-    contents=[Model(name='handy_closet_hook', file_name='handy_closet_hook_{}mm_bar'.format(str(bar_d).replace('.', 'dot')), bar_d=bar_d)
+    contents=[Model(name='handy_closet_hook', file_name='handy_closet_hook_{}mm_bar'.format(str(bar_d)), bar_d=bar_d)
               for bar_d in [4 + 0.5 * step for step in range(0, 7)]]
 )
 
