48179672600b7f90e738b32ce76273fc9f898d42
Charles Lenk <lenk.charles@gmail.com>
2026-01-11 15:40:01 -0800

    Updated the documentation to match template (#5)

--- export.py
+++ export.py
@@ -1,6 +1,6 @@
-from scad_export.export import export
-from scad_export.exportable import Folder, Model
+from scad_export.exportable import Folder, Model, export
 
+# Export a hook for multiple wire rod diameters in 0.5mm increments, starting at 4mm and ending at 7mm.
 files=Folder(
     name='scad_export/handy_closet_hook',
     contents=[Model(name='handy_closet_hook', rod_d=rod_d)
